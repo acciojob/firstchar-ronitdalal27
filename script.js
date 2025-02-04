ed4ea57aad1fb9462c7c3537d9ded5869a6bff24
ronitdalal27 <dalalronit131@gmail.com>
2025-02-04 10:52:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function firstChar(text) {
+function firstChar(str) {
   // your code here
+	var str1 = str.trim();
+	return str1.charAt(0);
+	
+	
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
